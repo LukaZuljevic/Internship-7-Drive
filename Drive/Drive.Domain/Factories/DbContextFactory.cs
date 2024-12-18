6e5a7adc567075a15735dc70274fae5d6ffd6f4b
zulja1 <luka.zuljo@gmail.com>
2024-12-18 11:58:35 +0100

    Add DbContext and Repository Factory

---
+++ Drive/Drive.Domain/Factories/DbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using Drive.Data.Entities;
+
+namespace Drive.Domain.Factories
+{
+    public static class DbContextFactory
+    {
+        public static DriveDbContext GetDriveDbContext()
+        {
+            var options = new DbContextOptionsBuilder()
+                .UseNpgsql(ConfigurationManager.ConnectionStrings["Drive"].ConnectionString)
+                .Options;
+
+            return new DriveDbContext(options);
+        }
+    }
+}
